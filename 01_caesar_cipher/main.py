2c35902104a4084da937454982b2e730d6b0e198
Oleksandr Petrov <ol.petrov.ua@gmail.com>
2026-08-17 21:25:29 +0300

    Add shift parameter and uppercase support

--- 01_caesar_cipher/main.py
+++ 01_caesar_cipher/main.py
@@ -1,6 +1,5 @@
-def caesar_cipher(text):
+def caesar_cipher(text, shift):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    shift = 4
     result = ''
 
     for char in text:
@@ -10,8 +9,6 @@ def caesar_cipher(text):
 
         char_index = alphabet.find(char.lower())
         new_char_index = (char_index + shift) % len(alphabet)
-        result += alphabet[new_char_index]
+        result += alphabet[new_char_index].upper() if char.isupper() else alphabet[new_char_index]
 
     return result
-
-
